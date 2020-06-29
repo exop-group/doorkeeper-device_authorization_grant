cb665f0fcf29f1353205bcc4f54835c91092bc69
Marco Nicola <m.nicola@exop-group.com>
2020-06-29 14:32:27 +0200

    Create DeviceAuthorization request strategy class

---
+++ lib/doorkeeper/device_authorization_grant/request/device_authorization.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Doorkeeper
+  module DeviceAuthorizationGrant
+    module Request
+      # Doorkeeper strategy for OAuth 2.0 Device Authorization Requests.
+      #
+      # @see https://tools.ietf.org/html/rfc8628#section-3.1 RFC 8628, sect. 3.1
+      class DeviceAuthorization < ::Doorkeeper::Request::Strategy
+        delegate :client, to: :server
+
+        # @return [OAuth::DeviceAuthorizationRequest]
+        def request
+          @request ||= OAuth::DeviceAuthorizationRequest.new(
+            Doorkeeper.configuration,
+            client,
+            host_name
+          )
+        end
+
+        private
+
+        # @return [String]
+        def host_name
+          req = server.context.request
+          "#{req.scheme}://#{req.host}#{port}"
+        end
+
+        # @return [String, nil]
+        def port
+          return nil if [80, 443].include?(server.context.request.port)
+
+          ":#{server.context.request.port}"
+        end
+      end
+    end
+  end
+end
