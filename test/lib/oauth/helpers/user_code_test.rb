3581f6aa70266359e79c39f74c32e2c66e5a2501
Marco Nicola <m.nicola@exop-group.com>
2020-06-29 15:34:52 +0200

    Add UserCode tests

---
+++ test/lib/oauth/helpers/user_code_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Doorkeeper
+  module DeviceAuthorizationGrant
+    module OAuth
+      module Helpers
+        class UserCodeTest < ActiveSupport::TestCase
+          test 'generates a string with 8 mixed digits and upcase chars' do
+            assert_match(/^[A-Z\d]{8}$/, UserCode.generate)
+          end
+
+          test 'generates a new random string at each call' do
+            first_code = UserCode.generate
+            10.times { refute_equal(first_code, UserCode.generate) }
+          end
+        end
+      end
+    end
+  end
+end
