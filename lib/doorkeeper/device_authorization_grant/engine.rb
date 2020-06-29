25c0d32a6456079a7ebc483f62caf3d61ed016b5
Marco Nicola <m.nicola@exop-group.com>
2020-06-29 14:44:19 +0200

    Add Rails routes installation to the Engine's initialization

--- lib/doorkeeper/device_authorization_grant/engine.rb
+++ lib/doorkeeper/device_authorization_grant/engine.rb
@@ -2,7 +2,11 @@
 
 module Doorkeeper
   module DeviceAuthorizationGrant
+    # Doorkeeper DeviceAuthorizationGrant Rails Engine
     class Engine < ::Rails::Engine
+      initializer 'doorkeeper.device_authorization_grant.routes' do
+        ::Doorkeeper::DeviceAuthorizationGrant::Rails::Routes.install!
+      end
     end
   end
 end
