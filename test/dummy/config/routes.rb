6f537d7681806265b215f4f5123d774d1f3d55f8
Marco Nicola <m.nicola@exop-group.com>
2020-06-29 15:17:31 +0200

    Install Doorkeeper::DeviceAuthorizationGrant in test dummy app
    $ bin/rails generate doorkeeper:device_authorization_grant:install

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   use_doorkeeper
+  use_doorkeeper_device_authorization_grant
 end
