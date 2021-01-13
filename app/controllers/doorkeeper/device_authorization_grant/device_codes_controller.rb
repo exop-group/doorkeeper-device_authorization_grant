29e6b7ada33d349507e80fe7dd155cc6b243e4d7
Marco Nicola <m.nicola@exop-group.com>
2021-01-13 13:02:17 +0100

    Fix class not found in DeviceCodesController#create

--- app/controllers/doorkeeper/device_authorization_grant/device_codes_controller.rb
+++ app/controllers/doorkeeper/device_authorization_grant/device_codes_controller.rb
@@ -9,7 +9,7 @@ module Doorkeeper
       def create
         headers.merge!(authorize_response.headers)
         render(json: authorize_response.body, status: authorize_response.status)
-      rescue Errors::DoorkeeperError => e
+      rescue Doorkeeper::Errors::DoorkeeperError => e
         handle_token_exception(e)
       end
 
